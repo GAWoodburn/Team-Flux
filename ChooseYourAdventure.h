947b6d603f47a4a7c71484d7f389ce18adf5390d
GAWoodburn <58796796+GAWoodburn@users.noreply.github.com>
2020-02-10 20:43:01 -0600

    Update ChooseYourAdventure.h

--- ChooseYourAdventure.h
+++ ChooseYourAdventure.h
@@ -1,15 +1,17 @@
-#pragma once
+#ifndef ChooseYourAdventure_H
+#define ChooseYourAdventure_H
 class ChooseYourAdventure{
 
 private:
 
 public:
 
-	ChooseYourAdventure(int &);
-
+	ChooseYourAdventure(int &); // Constructor called in main switch
+	~ChooseYourAdventure(){};   // Default Destructor
 	//Declare variables.
 	int startOrEndGame;
 	int personalityTestInput;
 
 };
 
+#endif
