ad756a499813c2ad002e8833771cdca2f3a13aaa
GAWoodburn <58796796+GAWoodburn@users.noreply.github.com>
2020-02-27 04:32:53 -0600

    Update ThrillRides.h

--- ThrillRides.h
+++ ThrillRides.h
@@ -11,14 +11,15 @@ class ThrillRide {
 private:
 
 public:
-	ThrillRide(int &playerMoney, bool &playerDeath);
+	ThrillRide(int& playerMoney, bool& playerDeath);
 	~ThrillRide();
-	void menu(char& decision, string& delimiter);
-	const int MOONMOUNTAIN = 10.00, DEVILSDROP = 8.00, TERRORTOWER = 5.00, TESTTRACK = 5.00, PENALTY = 5.00, REWARD = 10.00, ZERO = 0;  // Constants for prices
+	void menu(char& decision, string& delimiter, int&);
+	const int MOONMOUNTAIN = 10, DEVILSDROP = 8, TERRORTOWER = 5, TESTTRACK = 5, PENALTY = 5, REWARD = 10, ZERO = 0;  // Constants for prices
 	char decision, choice, resume;
 	string name, delimiter = "\n\n========================================================\n========================================================\n\n";  // String name and divider
 
 };
 
 #endif
+
 
