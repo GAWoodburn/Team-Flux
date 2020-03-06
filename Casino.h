108fc84303559a28ef8700c50897f68df123923e
Walkingdeadj <61568592+Walkingdeadj@users.noreply.github.com>
2020-03-06 08:45:45 -0600

    Update Casino.h

--- Casino.h
+++ Casino.h
@@ -1,18 +1,24 @@
 #ifndef Casino_H
 #define Casino_H
 
+#include<iostream>
+#include<math.h>
+#include<time.h> 
+
+using namespace std;
+
 class Casino{
 
 private:
-
+	
 public:
 
 	Casino();
-
 	int x = 1;
 	int rand(int n);
 	bool win();
 	void money();
+	char DisplayRule();
 
 
 };
