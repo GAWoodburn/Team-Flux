a261b84ebfa1f3b800e35954ff56b9af64678949
GAWoodburn <58796796+GAWoodburn@users.noreply.github.com>
2020-03-09 18:17:07 -0500

    Update Arcade.h

--- Arcade.h
+++ Arcade.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 class Arcade {
@@ -11,14 +12,13 @@ private:
 
 public:
 
-	Arcade();    // Constructor called in main switch 
-	~Arcade() {}; // Default destructor 
-	void menu(char& decision, string& delimiter);  // Menu function
-
-	double playerMoney = 100.00;  // Player money
-	const double COSTPACMAN = 10.00, COSTMORTALKOMBAT = 8.00, COSTASTEROIDS = 5.00, COSTMISSLECOMMAND = 5.00, PENALTY = 5.00, REWARD = 10.00;  // Constants for prices 
+	Arcade(int& playerMoney, bool& playerDeath);    // Constructor called in main switch 
+	~Arcade() {};								    // Default destructor 
+	void menu(char& decision, string& delimiter, int& playerMoney);   // Menu function
+	const int COSTPACMAN = 10, COSTMORTALKOMBAT = 8, COSTASTEROIDS = 5, COSTMISSLECOMMAND = 5, PENALTY = 5, REWARD = 10, ZERO = 0;  // Constants for prices 
 	char decision, choice, resume;  // Characters to guide pathways
 	string name, delimiter = "\n\n========================================================\n========================================================\n\n";  // String name and divider
 };
 
 #endif
+
